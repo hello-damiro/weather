292c4a7d6285b954fc64fe1ac899c572d535f84d
damiro <hello.damiro@gmail.com>
2023-04-11 15:19:51 +0800

    initial files and envi setup webpack

---
+++ README.md
@@ -0,0 +1,26 @@
+# weather
+
+The Odin Project - Intermediate JS project
+
+## Warning! This is a work in progress
+
+LIVE SITE [HERE](https://hello-damiro.github.io/weather)
+
+**Day 1:** Prepared design at figma. Focusing on the DOM elements that will use weather API. Preparing the design for future updates like unsplash API for the background images.
+
+</br>
+
+## Screenshot
+
+![Screenshot](https://github.com/hello-damiro/weather/blob/main/src/assets/images/screenshot.png?raw=true)
+
+</br>
+
+## Challenges
+
+1. Write the functions that hit the API. You’re going to want functions that can take a location and return the weather data for that location. For now, just `console.log()` the information.
+2. Write the functions that process the JSON data you’re getting from the API and return an object with only the data you require for your app.
+3. Set up a simple form that will let users input their location and will fetch the weather info (still just `console.log()` it).
+4. Display the information on your webpage! Add any styling you like!
+5. Optional: add a ‘loading’ component that displays from the time the form is submitted until the information comes back from the API.
+6. Push that baby to github and share your solution below!
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production', // environment: use 'development' if necessary. // use 'development' if necessary. Production mode will make the output files minified/uglified. Development mode, will not.
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'), // main source file
+    },
+    output: {
+        filename: '[name].js', // '[name]-[contenthash].js', // will inherit the name from the entry
+        path: path.resolve(__dirname, 'dist'),
+        clean: true, // delete exess files every issues of build command
+    },
+    devtool: 'source-map', // filename.map.js files use for debugging
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 8080,
+        open: true, // open browser
+        hot: true, // hot reload
+        liveReload: true, // live reload
+        compress: true, // enable gzip compression
+        historyApiFallback: true, // enable browser history fallback
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack Boilerplate', // Title that will reflect of HTML file
+            filename: 'index.html', // dist/index.html
+            template: 'src/template.html', // src/template.html
+            favicon: 'src/assets/images/favicon.png', // favicon
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                // HTML LOADER THAT CAN FETCH IMG IN ITS FILE
+                test: /\.html$/,
+                use: ['html-loader'],
+            },
+            {
+                // IMAGE LOADERS
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                // CSS LOADERS WITH MODULES
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1, // applies CSS modules on @imported resources
+                            modules: true, // enable CSS modules
+                        },
+                    },
+                ],
+                include: /\.module\.css$/,
+            },
+            {
+                // CSS LOADERS ONLY
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                exclude: /\.module\.css$/,
+            },
+            {
+                // BABEL LOADERS
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+};
